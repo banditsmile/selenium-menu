dcb26126f650906668add1bd6854ea7f43b79193
bandit <banditsmile@qq.com>
2023-08-28 17:36:29 +1000

    add readthedoc build requeirements

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ author = 'carl'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx-rtd-theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
